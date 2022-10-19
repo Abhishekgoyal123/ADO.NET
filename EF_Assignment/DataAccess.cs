c4841864aa981aa52af537df9ae871cf7341a3e4
Abhishek Goyal <abhishek.goyal@coditas.com>
2022-10-19 16:56:44 +0530

    19/10

---
+++ EF_Assignment/DataAccess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EF_Assignment.Models;
+
+namespace EF_Assignment
+{
+    
+    public class DataAccess
+    {
+        NorthwindContext context;
+
+        public DataAccess()
+        {
+            context = new NorthwindContext();
+        }
+
+        public void getData()
+        {
+            var data = from customer in context.Customers
+                       join order in context.Orders on customer.CustomerId equals order.CustomerId
+                       group customer by customer.CustomerId into newTable
+                       select new
+                       {
+                           id = newTable.Key,
+                           value = newTable.Count()
+                       };
+            foreach (var v in data)
+            {
+                Console.WriteLine($"{v.id} {v.value}");
+            }
+        }
+
+    }
+}
---
+++ EF_Assignment/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EF_Assignment;
+Console.WriteLine("Hello, World!");
+
+DataAccess da = new DataAccess();
+
+da.GetData_2();
