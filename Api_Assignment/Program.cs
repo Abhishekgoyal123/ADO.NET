239b9a76623e056cb8b5dbd366d8b5332234df93
Abhishek Goyal <abhishek.goyal@coditas.com>
2022-12-02 08:59:33 +0530

    02/12

--- Api_Assignment/Program.cs
+++ Api_Assignment/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using Api_Assignment.Models;
 using Api_Assignment;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +23,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<SecurityCodiDbContext>();
+
+builder.Services.AddScoped<SecurityService>();
 
 builder.Services.AddScoped<IDbAccessService<Category, int>, CategoryDataAccessService>();
 builder.Services.AddScoped<IDbAccessService<Product, int>, ProductDataAccessService>();
---
+++ MVC_1/FileService.cs
@@ -0,0 +1,46 @@
+﻿namespace MVC_1
+{
+    public class FileService : BackgroundService
+    {
+       
+        IWebHostEnvironment hostEnvironment;
+
+        public FileService(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+       
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            string PngPath = Path.Combine(hostEnvironment.WebRootPath, "PNGFiles");
+            string JPEGPath = Path.Combine(hostEnvironment.WebRootPath, "JPEGFiles");
+            
+            Directory.CreateDirectory(PngPath);
+            Directory.CreateDirectory(PngPath);
+
+            // string[] fileExtension = null;
+            string[] filePaths = Directory.GetFiles(Path.Combine(hostEnvironment.WebRootPath, "ReceivedFiles"));
+            
+            foreach(var item in filePaths)
+            {
+                string fileName = Path.GetFileName(item);
+                string fileExtension = Path.GetExtension(item);
+
+                if(fileExtension == ".png")
+                {
+                    string saveToPath = Path.Combine(PngPath, fileName);
+
+                    using (FileStream stream = new FileStream(saveToPath, FileMode.Create))
+                    {
+                        
+                        IFormFile file;
+                       file.CopyToAsync(stream);
+                    }
+
+                }
+            }
+
+        }
+    }
+}
